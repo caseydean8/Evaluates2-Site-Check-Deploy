aae678349ddd4e03c407e903e8849304254c8107
Casey Carroll <caseydean8@gmail.com>
2023-07-17 16:48:47 -0400

    test deploy with CNAME file in static folder

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 import styled from '@emotion/styled';
-import Headroom from "react-headroom";
+import Headroom from 'react-headroom';
 
-import { StaticQuery, graphql } from "gatsby";
-import Nav from "../Nav";
-import { SiteTitle, Label } from "./styles"
-import Evaluates2RoundedCornersImage from "../img-components/e2-rounded-corners.img";
-import mediaQuery from "../../utils/mediaQuery"
+import { StaticQuery, graphql } from 'gatsby';
+import Nav from '../Nav';
+import { SiteTitle, Label } from './styles';
+import Evaluates2RoundedCornersImage from '../img-components/e2-rounded-corners.img';
+import mediaQuery from '../../utils/mediaQuery';
 
-export const HeaderContainer = styled.header` 
-
-  background-color: ${props => props.path === '/' ? "rgba(0,0,0,0)" : '#000032'};
+export const HeaderContainer = styled.header`
+  background-color: ${props =>
+    props.path === '/' ? 'rgba(0,0,0,0)' : '#000032'};
 
   transition: top 1s linear;
   transition: opacity 0 linear;
@@ -22,65 +22,60 @@ export const HeaderContainer = styled.header`
   grid-gap: 20px;
   align-items: center;
   justify-items: center;
-  grid-template-areas: "nav title social search";
+  grid-template-areas: 'nav title social search';
   grid-template-columns: auto 1fr auto auto;
   font-weight: bold;
   ${mediaQuery.minNetbook} {
-    grid-template-areas: "title nav social search";
-  };
+    grid-template-areas: 'title nav social search';
+  }
 
   header {
     transition: ${props => props.theme.shortTrans};
   }
-  
-`
+`;
 
 const StyledLabel = styled(Label)`
   font-family: 'e2-Raleway-Bold';
-`
-
-const ColoredHeader = styled.div`
-
 `;
+
+const ColoredHeader = styled.div``;
 
 const StyledDImage = styled.div`
   min-width: calc(45px + 1vw);
   min-height: calc(45px + 1vw);
-`
+`;
 
 class Header extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       divStyle: {},
-      window: undefined
+      window: undefined,
     };
-
   }
 
   componentDidMount() {
-    this.setState({window: window, divStyle: this.state.divStyle})
+    this.setState({ window: window, divStyle: this.state.divStyle });
     var prevScrollpos = window.pageYOffset;
     var fromTopPx = 250;
-    window.onscroll = function () {
+    window.onscroll = function() {
       var scrolledFromtop = window.scrollY;
       if (scrolledFromtop > fromTopPx) {
         this.setState({
           divStyle: {},
-          window: this.state.window
-        })
+          window: this.state.window,
+        });
       } else {
         this.setState({
           divStyle: {},
-          window: this.state.window
-        })
+          window: this.state.window,
+        });
       }
       var currentScrollPos = window.pageYOffset;
       if (prevScrollpos > currentScrollPos) {
-        document.getElementsByTagName("header")[0].style.top = "0px"
+        document.getElementsByTagName('header')[0].style.top = '0px';
       } else {
-        document.getElementsByTagName("header")[0].style.top = "-300px"
+        document.getElementsByTagName('header')[0].style.top = '-300px';
       }
       prevScrollpos = currentScrollPos;
     }.bind(this);
@@ -109,17 +104,16 @@ class Header extends React.Component {
                     <Evaluates2RoundedCornersImage />
                   </StyledDImage>
                   <StyledLabel>
-                    (evaluates2) test
+                    (evaluates2) test deploy with CNAME file in static folder
                   </StyledLabel>
                 </SiteTitle>
                 <Nav />
               </HeaderContainer>
-            </ ColoredHeader>
+            </ColoredHeader>
           </Headroom>
-        )
-        }
+        )}
       />
-    )
+    );
   }
 }
 
